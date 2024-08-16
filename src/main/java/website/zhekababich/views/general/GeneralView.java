33b2c3aa5baa19bd18bb67cda939004943f249a0
start.vaadin.com <no-reply@vaadin.com>
2024-08-16 11:34:05 +0000

    Generated project

---
+++ src/main/java/website/zhekababich/views/general/GeneralView.java
@@ -0,0 +1,50 @@
+package website.zhekababich.views.general;
+
+import com.vaadin.flow.component.Composite;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
+import com.vaadin.flow.component.orderedlayout.FlexComponent.JustifyContentMode;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+import com.vaadin.flow.theme.lumo.LumoUtility.Gap;
+import website.zhekababich.views.MainLayout;
+
+@PageTitle("General")
+@Route(value = "", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class GeneralView extends Composite<VerticalLayout> {
+
+    public GeneralView() {
+        HorizontalLayout layoutRow2 = new HorizontalLayout();
+        HorizontalLayout layoutRow = new HorizontalLayout();
+        VerticalLayout layoutColumn2 = new VerticalLayout();
+        VerticalLayout layoutColumn3 = new VerticalLayout();
+        VerticalLayout layoutColumn4 = new VerticalLayout();
+        HorizontalLayout layoutRow3 = new HorizontalLayout();
+        getContent().setWidth("100%");
+        getContent().getStyle().set("flex-grow", "1");
+        layoutRow2.addClassName(Gap.MEDIUM);
+        layoutRow2.setWidth("100%");
+        layoutRow2.setHeight("min-content");
+        layoutRow2.setAlignItems(Alignment.CENTER);
+        layoutRow2.setJustifyContentMode(JustifyContentMode.CENTER);
+        layoutRow.addClassName(Gap.MEDIUM);
+        layoutRow.setWidth("100%");
+        layoutRow.getStyle().set("flex-grow", "1");
+        layoutColumn2.getStyle().set("flex-grow", "1");
+        layoutColumn3.setWidth("100%");
+        layoutColumn3.getStyle().set("flex-grow", "1");
+        layoutColumn4.getStyle().set("flex-grow", "1");
+        layoutRow3.addClassName(Gap.MEDIUM);
+        layoutRow3.setWidth("100%");
+        layoutRow3.setHeight("min-content");
+        getContent().add(layoutRow2);
+        getContent().add(layoutRow);
+        layoutRow.add(layoutColumn2);
+        layoutRow.add(layoutColumn3);
+        layoutRow.add(layoutColumn4);
+        getContent().add(layoutRow3);
+    }
+}
